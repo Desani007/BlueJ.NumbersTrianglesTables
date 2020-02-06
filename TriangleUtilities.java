b97484a0882000b019108b72a3e6be3861f9e7e5
Destiny Bond <dbond@zipcodes-MacBook-Pro-51.local>
2020-02-06 15:59:57 -0500

    added Triangle UTIL test

--- TriangleUtilities.java
+++ TriangleUtilities.java
@@ -3,19 +3,38 @@
 public class TriangleUtilities {
 
     public static String getRow(int numberOfStars) {
-        return null;
+        String stars="";
+        for ( int i=1; i<=numberOfStars;i++){
+            stars+="*";
+        }
+        return stars;
     }
     
     public static String getTriangle(int numberOfRows) {
-        return null;
+       String stars="";
+     
+        for ( int i=1; i<=numberOfRows;i++){
+            stars+=getRow(i) +"\n";
+            }
+        return stars;
     }
 
 
     public static String getSmallTriangle() {
-        return null;
+       String stars="";
+     
+        for ( int i=1; i<=4;i++){
+            stars+=getRow(i) +"\n";
+            }
+        return stars;
     }
 
     public static String getLargeTriangle() {
-        return null;
+       String stars="";
+     
+        for ( int i=1; i<=9;i++){
+            stars+=getRow(i) +"\n";
+            }
+        return stars;
     }
 }
