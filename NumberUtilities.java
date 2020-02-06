d3a4a594d9c0bd237f82bba2a06e02b1a1cf9fab
Destiny Bond <dbond@zipcodes-MacBook-Pro-51.local>
2020-02-06 13:22:02 -0500

    NumberUtilites test completed

--- NumberUtilities.java
+++ NumberUtilities.java
@@ -4,29 +4,68 @@
 public class NumberUtilities {
 
     public static String getRange(int stop) {
-        return null;
+        String string ="";
+    for ( int i=0; i<stop;i++){
+      string+=i ;
+        }
+        return string;
     }
 
     public static String getRange(int start, int stop) {
-        return null;
+        String string ="";
+      for ( int i=start; i<stop;i++){
+         string+=i ;
+        }
+      
+        return string;
     }
 
 
     public static String getRange(int start, int stop, int step) {
-        return null;
+         String string ="";
+         
+      for ( int i=start; i<stop; i+=step){
+         string+=i;
+        
+        }
+      
+        return string;
     }
 
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+        String string ="";
+         
+      for ( int i=start; i<stop; i++){
+          if (i%2!=0) continue;{
+         string+=i;
+        }
+        
+        }
+      
+        return string;
     }
 
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+         String string ="";
+         
+      for ( int i=start; i<stop; i++){
+          if (i%2!=0){
+         string+=i;
+        }
+    }
+        return string;
     }
 
 
     public static String getExponentiations(int start, int stop, int exponent) {
-        return null;
+     String string ="";
+     for ( int i=start; i <=stop; i++){
+       int num=(int)Math.pow(i, exponent);
+       string+=num;
+       
     }
+    return string;  
+}
+
 }
